e27fe6d809e5481024b56aad935f194765aea3b0
Olena Titiyevska <leenaleelab@gmail.com>
2025-10-18 17:25:39 +0300

    add_new_developer

--- total_salary_01.py
+++ total_salary_01.py
@@ -1,5 +1,7 @@
+# Спочатку додаємо новий рядок
 with open("list_of_developers.txt", "a") as f:
-    f.write("Serj Griznov,2500\n")
+    f.write("\nSerj Griznov,2500\n")
 
+# Потім читаємо увесь файл
 with open("list_of_developers.txt", "r") as f:
     print(f.read())
