8ab267f838e1ccc54df2a28e3cb15c88d82b4471
Olena Titiyevska <leenaleelab@gmail.com>
2025-10-20 05:49:41 +0300

    colorama_check

--- colorama_dir.py
+++ colorama_dir.py
@@ -18,3 +18,19 @@ import sys
 from colorama import Fore, Back, Style, init
 
 init(autoreset=True)
+
+def show_structure(): #функція - отримує шлях, перевіряє його, виводить структуру
+    if len(sys.argv) < 2: # перевірка, чи передав користувач шлях   
+                          # до діректорії
+        print("Будь ласка, вкажіть шлях до директорії.")
+        return
+
+    folder_path = Path(sys.argv[1])
+
+    if not folder_path.exists():
+        print(Fore.RED + "Помилка: вказаний шлях не існує!")
+        return
+    if not folder_path.is_dir():
+        print(Fore.RED + "Помилка: це не директорія!")
+        return
+    print(Style.BRIGHT + Fore.MAGENTA + f"\nСтруктура директорії: {folder_path}\n")
